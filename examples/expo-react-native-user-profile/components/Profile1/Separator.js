e41cebb2db4ad9e421cc6ea25935754d8879c108
nattatorn.dev <nattatorn.dev@gmail.com>
2017-10-25 00:48:38 +0700

    🎨 Refactor

--- examples/expo-react-native-user-profile/components/Profile1/Separator.js
+++ examples/expo-react-native-user-profile/components/Profile1/Separator.js
@@ -1,30 +1,28 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import PropTypes from 'prop-types'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 
 const Separator = () => (
   <View style={styles.separatorContainer}>
     <View style={styles.separatorOffset} />
-    <View
-      style={styles.separator}
-    />
+    <View style={styles.separator} />
   </View>
-);
+)
 
 const styles = StyleSheet.create({
   separatorContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   separatorOffset: {
-    flexDirection: "row",
+    flexDirection: 'row',
     flex: 2,
   },
   separator: {
-    flexDirection: "row",
+    flexDirection: 'row',
     flex: 8,
-    borderColor: "#EDEDED",
+    borderColor: '#EDEDED',
     borderWidth: 0.8,
   },
-});
+})
 
-export default Separator;
+export default Separator
