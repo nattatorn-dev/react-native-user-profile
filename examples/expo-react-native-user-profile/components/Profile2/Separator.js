9c3420255aa1224b4a140f823de61118b93799f7
nattatorn.dev <nattatorn.dev@gmail.com>
2017-11-17 21:45:15 +0700

    Add Separator

---
+++ examples/expo-react-native-user-profile/components/Profile2/Separator.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+const Separator = () => (
+  <View style={styles.separatorContainer}>
+    <View style={styles.separator} />
+  </View>
+)
+
+const styles = StyleSheet.create({
+  separatorContainer: {
+    flexDirection: 'row',
+  },
+  separatorOffset: {
+    flexDirection: 'row',
+    flex: 2,
+  },
+  separator: {
+    flexDirection: 'row',
+    flex: 8,
+    borderColor: '#ACB1AD',
+    borderWidth: 0.8,
+  },
+})
+
+export default Separator
