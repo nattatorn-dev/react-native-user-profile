5edbe66b3170adb7bcacd7cb89c7693677aa6799
Alex Rich <arich@hmc.edu>
2021-10-01 19:57:58 -0700

    Update Expo SDK to be compatible with latest Expo Go App

--- screens/Product1/index.js
+++ screens/Product1/index.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-import productData from './product.json'
+import productData from './product.json';
 
-import { NavAbsolute } from '../../components'
-import Product from './Product'
+import { NavAbsolute } from '../../components';
+import Product from './Product';
 
 const ProductScreen = (props) => {
-  props.navigation.setOptions({
-    header: ({ navigation }) => (
-        <NavAbsolute
-          navigation={navigation}
-          title={productData.title}
-          subTitle={productData.address}
-        />
-    ),
-  })
+  useEffect(
+    () =>
+      props.navigation.setOptions({
+        header: ({ navigation }) => (
+          <NavAbsolute
+            navigation={navigation}
+            title={productData.title}
+            subTitle={productData.address}
+          />
+        ),
+      }),
+    []
+  );
 
-  return <Product {...productData} {...props}/>
-}
+  return <Product {...productData} {...props} />;
+};
 
-export default ProductScreen
+export default ProductScreen;
--- screens/Setting1/Options.js
+++ screens/Setting1/Options.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { SafeAreaView, View } from 'react-native'
+import React, { useEffect } from 'react';
+import { SafeAreaView, View } from 'react-native';
 
-import { Nav } from '../../components'
+import { Nav } from '../../components';
 
 const Options = (props) => {
-  props.navigation.setOptions({
-    header: ({navigation}) => (
-      <SafeAreaView>
-        <Nav title="Settings" navigation={navigation} />
-      </SafeAreaView>
-    ),
-  })
+  useEffect(() => {
+    props.navigation.setOptions({
+      header: ({ navigation }) => (
+        <SafeAreaView>
+          <Nav title="Settings" navigation={navigation} />
+        </SafeAreaView>
+      ),
+    });
+  }, []);
 
-  return <View {...props} />
-}
+  return <View {...props} />;
+};
 
-export default Options
+export default Options;
